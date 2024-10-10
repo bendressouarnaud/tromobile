c84c83ee4857f91bbc75b250def8d8fe5be3de6a
N-GBANDAMA <arnaud.ngbandama@gcatrans.com>
2024-10-10 14:42:05 +0200

    10102024 Sauvegarde

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -12,16 +12,19 @@ class DatabaseHelper {
 
   // This is the actual database filename that is saved in the docs directory.
   static final _databaseName = "fluttercommerce.db";
+
   // Increment this version when you need to change the schema.
   static final _databaseVersion = 1;
 
 
   // Make this a singleton class.
   DatabaseHelper._privateConstructor();
+
   static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
 
   // Only allow a single open connection to the database.
-  static Database? _database ;
+  static Database? _database;
+
   Future<Database> get database async {
     if (_database != null) return _database!;
     _database = await _initDatabase();
@@ -35,40 +38,67 @@ class DatabaseHelper {
     String path = join(documentsDirectory.path, _databaseName);
     // Open the database. Can also add an onUpdate callback parameter.
     return await openDatabase(path,
-        version: _databaseVersion,
-        onCreate: _onCreate);
+      version: _databaseVersion,
+      onCreate: _onCreate,
+      onUpgrade: _onUpgrade
+    );
   }
 
-  // SQL string to create the database
-  Future _onCreate(Database db, int version) async {
-    await db.execute('CREATE TABLE user (id INTEGER PRIMARY KEY,typepieceidentite TEXT,numeropieceidentite TEXT,'
-        'nom TEXT, prenom TEXT, email TEXT,numero TEXT,adresse TEXT,fcmtoken TEXT,pwd TEXT, codeinvitation TEXT,'
-        'nationnalite TEXT, villeresidence INTEGER)');
-    await db.execute('CREATE TABLE pays (id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,iso2 TEXT,iso3 TEXT,unicodeFlag TEXT)');
+  Future _onCreate(Database db, int newVersion) async {
+    for (int version = 0; version < newVersion; version++) {
+      await _performDbOperationsVersionWise(db, version + 1);
+    }
+  }
 
-    await db.execute('CREATE TABLE publication (id INTEGER PRIMARY KEY,userid INTEGER,villedepart INTEGER,'
-        'villedestination INTEGER, datevoyage TEXT, datepublication TEXT,reserve INTEGER,active INTEGER,reservereelle INTEGER,souscripteur INTEGER,'
-        'milliseconds INTEGER, identifiant TEXT, devise INTEGER, prix INTEGER, read INTEGER)');
+  Future _onUpgrade(Database db, int oldVersion, int newVersion) async {
+    for (int version = 0; version < newVersion; version++) {
+      await _performDbOperationsVersionWise(db, version + 1);
+    }
+  }
 
-    await db.execute('CREATE TABLE ville (id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,paysid INTEGER)');
-    await db.execute('CREATE TABLE cible (id INTEGER PRIMARY KEY ,villedepartid INTEGER,paysdepartid INTEGER,'
-        'villedestid INTEGER,paysdestid INTEGER, topic TEXT)');
-    await db.execute('CREATE TABLE chat (id INTEGER PRIMARY KEY AUTOINCREMENT,idpub INTEGER,milliseconds INTEGER,'
-        'sens INTEGER,statut INTEGER, contenu TEXT, identifiant TEXT, iduser INTEGER, idlocaluser INTEGER, read INTEGER)');
+  _performDbOperationsVersionWise(Database db, int version) async {
+    switch (version) {
+      case 1:
+        await _createDatabase(db);
+        break;
+    }
+  }
 
-    await db.execute('CREATE TABLE souscription (id INTEGER PRIMARY KEY AUTOINCREMENT,idpub INTEGER,iduser INTEGER,'
-        'millisecondes INTEGER, reserve INTEGER, statut INTEGER)');
+  Future _createDatabase(Database db) async {
+    await db.execute(
+        'CREATE TABLE user (id INTEGER PRIMARY KEY,typepieceidentite TEXT,numeropieceidentite TEXT,'
+            'nom TEXT, prenom TEXT, email TEXT,numero TEXT,adresse TEXT,fcmtoken TEXT,pwd TEXT, codeinvitation TEXT,'
+            'nationnalite TEXT, villeresidence INTEGER)');
+    await db.execute(
+        'CREATE TABLE pays (id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,iso2 TEXT,iso3 TEXT,unicodeFlag TEXT)');
 
-    await db.execute('CREATE TABLE parameters (id INTEGER PRIMARY KEY,state TEXT,travellocal INTEGER,'
-        'travelabroad INTEGER, notification INTEGER, epochdebut INTEGER, epochfin INTEGER)');
+    await db.execute(
+        'CREATE TABLE publication (id INTEGER PRIMARY KEY,userid INTEGER,villedepart INTEGER,'
+            'villedestination INTEGER, datevoyage TEXT, datepublication TEXT,reserve INTEGER,active INTEGER,reservereelle INTEGER,souscripteur INTEGER,'
+            'milliseconds INTEGER, identifiant TEXT, devise INTEGER, prix INTEGER, read INTEGER)');
 
-    await db.execute('CREATE TABLE filiation (id INTEGER PRIMARY KEY,code TEXT,bonus REAL)');
+    await db.execute(
+        'CREATE TABLE ville (id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,paysid INTEGER)');
+    await db.execute(
+        'CREATE TABLE cible (id INTEGER PRIMARY KEY ,villedepartid INTEGER,paysdepartid INTEGER,'
+            'villedestid INTEGER,paysdestid INTEGER, topic TEXT)');
+    await db.execute(
+        'CREATE TABLE chat (id INTEGER PRIMARY KEY AUTOINCREMENT,idpub INTEGER,milliseconds INTEGER,'
+            'sens INTEGER,statut INTEGER, contenu TEXT, identifiant TEXT, iduser INTEGER, idlocaluser INTEGER, read INTEGER)');
+
+    await db.execute(
+        'CREATE TABLE souscription (id INTEGER PRIMARY KEY AUTOINCREMENT,idpub INTEGER,iduser INTEGER,'
+            'millisecondes INTEGER, reserve INTEGER, statut INTEGER)');
+
+    await db.execute(
+        'CREATE TABLE parameters (id INTEGER PRIMARY KEY,state TEXT,travellocal INTEGER,'
+            'travelabroad INTEGER, notification INTEGER, epochdebut INTEGER, epochfin INTEGER)');
+
+    await db.execute(
+        'CREATE TABLE filiation (id INTEGER PRIMARY KEY,code TEXT,bonus REAL)');
 
     // Init values :
     _createCountry(db);
-
-    /*await db.execute('CREATE TABLE article (idart INTEGER PRIMARY KEY,iddet INTEGER,prix INTEGER, reduction INTEGER, note INTEGER, articlerestant INTEGER, libelle TEXT, lienweb TEXT)');*/
-    //await db.execute('CREATE TABLE user (id INTEGER PRIMARY KEY,name TEXT NOT NULL,pwd TEXT NOT NULL)');
   }
 
   Future<void> _createCountry(Database database) async {
